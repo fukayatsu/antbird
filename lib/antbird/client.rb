9111feeb5f3ec0b251c63c2a6bdc665cda43fe2a
fukayatsu <fukayatsu@gmail.com>
2018-03-30 17:19:33 +0900

    wip

--- lib/antbird/client.rb
+++ lib/antbird/client.rb
@@ -1,54 +1,92 @@
+require 'faraday'
+require 'faraday_middleware'
+
 module Antbird
   class Client
-    def initialize(url: "http://localhost:9200", version: '6.2.3')
-      @url = url
-      require 'antbird/client/rest_api/rest_api_v5_5'
+    def initialize(
+      scope = {},
+      url: "http://localhost:9200",
+      version: '5.5.3',
+      read_timeout: 5,
+      open_timeout: 2
+    )
+      @scope     = {}
+      @url       = url
+      @version   = version
+
+      @read_timeout = read_timeout
+      @open_timeout = open_timeout
+
+      @api_specs = {}
+
+      class_version = version.split('.')[0,2].join('_')
+
+      require "antbird/rest_api/rest_api_v#{class_version}"
+      extend Antbird::RestApi.const_get "RestApiV#{class_version}"
+    end
+    attr_reader :scope, :url, :version
+    attr_reader :read_timeout, :open_timeout
+    attr_reader :api_specs
+
+    def scope(new_scope)
+      Client.new(new_scope, url: @url, version: @version)
     end
 
-    def get(path, params = {})
-      request :get, path, params
-    end
+    def request(api_spec, params)
+      # TODO: embed
+      path   = api_spec['url']['paths'].find { |path| path.count('{') == @scope.count  }
+      method = api_spec['methods'].first.downcase.to_sym
 
-    def put(path, params = {})
-      request :put, path, params
-    end
-
-    def post(path, params = {})
-      request :post, path, params
-    end
-
-    def delete(path, params = {})
-      request :delete, path, params
-    end
-
-    def request(method)
       read_timeout = params.delete(:read_timeout)
+      body = extract_body(params)
+
       response =
         case method
         when :head
           connection.head(path) { |req| req.options[:timeout] = read_timeout if read_timeout }
-
         when :get
           connection.get(path) { |req|
             req.body = body if body
             req.options[:timeout] = read_timeout if read_timeout
           }
-
-        when :put
-          connection.put(path, body) { |req| req.options[:timeout] = read_timeout if read_timeout }
-
-        when :post
-          connection.post(path, body) { |req| req.options[:timeout] = read_timeout if read_timeout }
-
-        when :delete
-          connection.delete(path) { |req|
-            req.body = body if body
-            req.options[:timeout] = read_timeout if read_timeout
-          }
-
+        # when :post # TODO
         else
           raise ArgumentError, "unknown HTTP request method: #{method.inspect}"
         end
+
+      response.body
+    end
+
+    def extract_body(params)
+      body = params.delete :body
+      return if body.nil?
+
+      body =
+        case body
+        when String
+          body
+        when Array
+          body << nil unless body.last.nil?
+          body.join "\n"
+        else
+          JSON.dump(body)
+        end
+
+      # Prevent excon from changing the encoding (see https://github.com/github/elastomer-client/issues/138)
+      body.freeze
+    end
+
+    def connection
+      @connection ||= Faraday.new(url) do |conn|
+        # TODO: bulkのところはndjsonなのでこれだとダメかも
+        conn.request :json
+        conn.response :json, :content_type => /\bjson$/
+
+        conn.options[:timeout]      = read_timeout
+        conn.options[:open_timeout] = open_timeout
+
+        conn.adapter Faraday.default_adapter
+      end
     end
   end
 end
--- spec/lib/antbird/client_spec.rb
+++ spec/lib/antbird/client_spec.rb
@@ -16,7 +16,30 @@ RSpec.describe Antbird::Client do
     end
   end
 
-  describe '#cluster' do
-    
+  describe 'api methods' do
+    let(:client) { described_class.new }
+    describe '#_cluster_health' do
+      subject { client._cluster_health }
+
+      it do
+        expect(subject.keys).to match_array([
+          "cluster_name",
+          "status",
+          "timed_out",
+          "number_of_nodes",
+          "number_of_data_nodes",
+          "active_primary_shards",
+          "active_shards",
+          "relocating_shards",
+          "initializing_shards",
+          "unassigned_shards",
+          "delayed_unassigned_shards",
+          "number_of_pending_tasks",
+          "number_of_in_flight_fetch",
+          "task_max_waiting_in_queue_millis",
+          "active_shards_percent_as_number"
+        ])
+      end
+    end
   end
 end
